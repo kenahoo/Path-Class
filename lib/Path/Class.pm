3fd9fa74599bd23cfcb238bc30904a8f5634c6a8
cho45 <cho45@lowreal.net>
2012-07-20 16:50:11 +0900

    add new `tempdir` function to Path::Class which returns new Path::Class::Dir instance pointed to temporary directory.

--- lib/Path/Class.pm
+++ lib/Path/Class.pm
@@ -7,17 +7,19 @@ package Path::Class;
   no strict 'vars';
   @ISA = qw(Exporter);
   @EXPORT    = qw(file dir);
-  @EXPORT_OK = qw(file dir foreign_file foreign_dir);
+  @EXPORT_OK = qw(file dir foreign_file foreign_dir tempdir);
 }
 
 use Exporter;
 use Path::Class::File;
 use Path::Class::Dir;
+use File::Temp ();
 
 sub file { Path::Class::File->new(@_) }
 sub dir  { Path::Class::Dir ->new(@_) }
 sub foreign_file { Path::Class::File->new_foreign(@_) }
 sub foreign_dir  { Path::Class::Dir ->new_foreign(@_) }
+sub tempdir { Path::Class::Dir->new(File::Temp::tempdir(@_)) }
 
 
 1;
